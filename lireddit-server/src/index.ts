8fa704a3d5a93511e59cae668d463f1ac93bada4
minhtrannhat <minhtrannhat2001@gmail.com>
2021-03-10 17:52:21 -0500

    Prep work for graphql apollo integrations

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -2,18 +2,32 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config"
+import express from 'express';
+import {} from 'apollo-server-express';
 
 const main = async () => {
     // connect to database
     const orm = await MikroORM.init(microConfig);
 
     // run migrations
-    orm.getMigrator().up();
+    // orm.getMigrator().up();
+
+    const app = express();
+
+    app.get('/', (_, res) => {
+        res.send("hello world!");
+    })
+    app.listen(
+        4000, () => {
+            console.log('server started on localhost:4000')
+        }
+    )
+
     // create a post 
     // ! This does not actually insert a post into the database
     // ! This is just the same as `const post = new Post()`
-    const create_post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(create_post);
+    // const create_post = orm.em.create(Post, {title: 'my first post'});
+    // await orm.em.persistAndFlush(create_post);
     const posts = await orm.em.find(Post, {});
     console.log(posts);
 }
---
+++ lireddit-server/src/resolvers/hello.ts
@@ -0,0 +1,8 @@
+import { Resolver } from "dns";
+
+import { Resolver, Query } from "type-graphql";
+
+@Resolver()
+export class HelloResolver {
+    
+}
