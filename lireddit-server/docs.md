3b80c6025bad909e71d3606c725d60114e1cc85e
minhtrannhat <minhtrannhat2001@gmail.com>
2021-03-13 18:44:15 -0500

    Prettier every files

--- lireddit-server/docs.md
+++ lireddit-server/docs.md
@@ -3,10 +3,11 @@
 Mikro-orm is basically just a way we can interact with the Postgresql database using Typescript.
 
 ## How to make types more specific in Typescript
-by casting the object/type as const 
+
+by casting the object/type as const
 
 ```typescript
 export default {
-    type: "asdf"
-} as const
+  type: "asdf",
+} as const;
 ```
--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,49 +1,48 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import microConfig from "./mikro-orm.config"
-import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
-import {buildSchema} from 'type-graphql'
-import { HelloResolver } from "./resolvers/hello";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-    // connect to database
-    const orm = await MikroORM.init(microConfig);
+  // connect to database
+  const orm = await MikroORM.init(microConfig);
 
-    // run migrations
-    // orm.getMigrator().up();
+  // run migrations
+  // orm.getMigrator().up();
 
-    const app = express();
+  const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver],
-            validate: false   
-        }
-        )
-    });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
 
-    apolloServer.applyMiddleware({app});
+  apolloServer.applyMiddleware({ app });
 
-    app.get('/', (_, res) => {
-        res.send("hello world!");
-    })
-    app.listen(
-        4000, () => {
-            console.log('server started on localhost:4000')
-        }
-    )
+  app.get("/", (_, res) => {
+    res.send("hello world!");
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 
-    // create a post 
-    // ! This does not actually insert a post into the database
-    // ! This is just the same as `const post = new Post()`
-    // const create_post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(create_post);
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
-}
+  // create a post
+  // ! This does not actually insert a post into the database
+  // ! This is just the same as `const post = new Post()`
+  // const create_post = orm.em.create(Post, {title: 'my first post'});
+  // await orm.em.persistAndFlush(create_post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
+};
 
 main().catch((err) => {
-    console.log(err);
+  console.log(err);
 });
