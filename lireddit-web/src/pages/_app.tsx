f722a48c79c4fac6f2a3a3dc2f0ee80a67e5e3a7
Minh Tran Nhat <minhradz@gmail.com>
2021-05-13 16:22:14 -0400

    Urql cache update (#14)
    * urql caching template
    - Finished urql caching update so that users can logout smoothly
    - Updated deps for both backend and frontend
    - Added a graphql fragment for the User type

--- lireddit-web/src/pages/_app.tsx
+++ lireddit-web/src/pages/_app.tsx
@@ -1,16 +1,76 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { cacheExchange, QueryInput, Cache } from "@urql/exchange-graphcache";
+import {
+  MeDocument,
+  MeQuery,
+  LoginMutation,
+  RegisterMutation,
+} from "../generated/graphql";
+import theme from "../theme";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://127.0.0.1:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
-  exchanges: [dedupExchange, cacheExchange({}), fetchExchange],
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery<RegisterMutation, MeQuery>(
+            cache,
+            { query: MeDocument },
+            _result,
+            (result, query) => {
+              if (result.register.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.register.user,
+                };
+              }
+            }
+          );
+        },
+        Subscription: {
+          subscriptionField: (result, args, cache, info) => {
+            // ...
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (
